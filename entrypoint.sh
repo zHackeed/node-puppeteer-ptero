de2d109ee0f49630d1dc2f1f93e74ad79a98fc82
Alejandro G <58375195+zHackeed@users.noreply.github.com>
2022-12-30 02:48:38 +0100

    fix variables not being replaced

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ export INTERNAL_IP
 
 node -v
 
-eval ${STARTUP}
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e "STARTUP: ${MODIFIED_STARTUP}"
+
+eval ${MODIFIED_STARTUP}
